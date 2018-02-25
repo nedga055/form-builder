19b5718c28d5bb194e5f20360e1fb2cc626f6aa1
Nick Edgar <edgar@76design.com>
2018-02-24 20:21:29 -0500

    Constant for holding component types.

---
+++ src/components/FormComponents/ComponentTypes.js
@@ -0,0 +1,13 @@
+export const COMPONENT_TYPES = [{
+	value: '',
+	text: 'Select a component'
+},{
+	value: 'text',
+	text: 'Textfield'
+},{
+	value: 'email',
+	text: 'Email'
+},{
+	value: 'select-list',
+	text: 'Select List'
+}];
